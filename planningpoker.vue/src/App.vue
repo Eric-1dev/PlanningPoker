e8fd2594ad87b4f89ac6aad2256b93f480f6eaf7
Alexey Anisimov <eric-1@yandex.ru>
2023-09-11 23:47:33 +0400

    добавил пустой проект

---
+++ planningpoker.vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>ddd={{ likes }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 1
+        }
+    }
+}
+</script>
+
+<style></style>
